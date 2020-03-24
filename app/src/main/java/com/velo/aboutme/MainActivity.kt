73fd76e5da1f98c526d2027c3df56a9dd110dc90
Shrish2784 <shrishasthana@outlook.com>
2020-03-24 14:15:48 +0530

    Added Databinding to access views

--- app/src/main/java/com/velo/aboutme/MainActivity.kt
+++ app/src/main/java/com/velo/aboutme/MainActivity.kt
@@ -4,31 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.velo.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val nickNameInput: EditText = findViewById<EditText>(R.id.nickNameInput);
-        val nickName: TextView = findViewById<EditText>(R.id.nickName);
-
-        nickNameInput.setOnKeyListener(View.OnKeyListener { v: View?, keyCode: Int, event: KeyEvent? ->
+        binding.nickNameInput.setOnKeyListener(View.OnKeyListener { v: View?, keyCode: Int, event: KeyEvent? ->
             if ((event?.action == KeyEvent.ACTION_DOWN) and (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                nickNameInput.visibility = View.GONE
-                nickName.text = nickNameInput.text
-                nickName.visibility = View.VISIBLE
+                binding.nickNameInput.visibility = View.GONE
+                binding.nickName.text = binding.nickNameInput.text
+                binding.nickName.visibility = View.VISIBLE
                 return@OnKeyListener true
             }
             return@OnKeyListener false
         })
 
-        nickName.setOnClickListener { v: View ->
-            nickName.visibility = View.GONE
-            nickNameInput.visibility = View.VISIBLE
+        binding.nickName.setOnClickListener { v: View ->
+            binding.nickName.visibility = View.GONE
+            binding.nickNameInput.visibility = View.VISIBLE
         }
     }
 }
